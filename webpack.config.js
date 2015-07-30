640f13a9662eef6c84eba6bf6e467d16765f76fd
Evan Johnson <evan@createinform.com>
2015-07-29 21:12:54 -0700

    Setup webpack and work flow. Install React and other npm modules.

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// Add WebPack to use the included CommonsChunkPlugin
+var webpack = require('webpack');
+var bower_dir = __dirname + '/bower_components';
+
+var config = {
+   addVendor: function (name, path) {
+    this.resolve.alias[name] = path;
+    this.module.noParse.push(path);
+  },
+
+  // We split the entry into two specific chunks. Our app and vendors. Vendors
+  // specify that react should be part of that chunk
+  entry: {
+    app: process.env.NODE_ENV === 'production' ? ['./app/main.js'] : ['webpack/hot/dev-server', './app/main.js']
+  },
+  resolve: { alias: {} },
+
+  // We add a plugin called CommonsChunkPlugin that will take the vendors chunk
+  // and create a vendors.js file. As you can see the first argument matches the key
+  // of the entry, "vendors"
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('app', null, false)
+  ],
+  output: {
+    publicPath: '/',
+    path: process.env.NODE_ENV === 'production' ? './dist' : './build',
+    filename: 'bundle.js'
+  },
+  module: {
+    noParse: [],
+    loaders: [
+      { test: /\.js$/, loader: 'jsx-loader' }
+    ]
+  }
+};
+
+config.addVendor('react', bower_dir + '/react/react.min.js');
+
+module.exports = config;
+
