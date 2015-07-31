bd74b8274c319ea973e4f0b7e23ff036c6c97f1f
Evan Johnson <evajohns@amazon.com>
2015-07-30 20:46:00 -0700

    Add github utils.

---
+++ app/utils/githubUtils.js
@@ -0,0 +1,10 @@
+var axios = require('axios');
+
+var githubUtils = {
+  getUserInfo: function() {
+    console.log('getting user info');
+    return 'say what!?';
+  }
+}
+
+module.exports = githubUtils;
