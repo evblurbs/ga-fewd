6a316cc44afec97a6469a523a13a19719676a174
Evan Johnson <evan@createinform.com>
2015-08-11 00:53:10 -0700

    Update app constants for github email call.

--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -2,7 +2,7 @@ var appConstants = {
   GITHUB_HOSTNAME: 'api.github.com',
   GITHUB_API_USER: '/user?access_token=',
   GITHUB_API_REPOS: '/user/repo?access_token=',
-  GITHUB_API_EMAILS: '/user/emails?access_token=',
+  GITHUB_API_EMAILS: 'https://api.github.com/user/emails?access_token=',
   GITHUB_API_SCOPE: 'user,user:email,repo',
   GITHUB_UA: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
   PARSE_APP_ID: 'AaX7jjvYNO0hc0DAjy7EHOj3jDvovR4z7TVPHOyq',
