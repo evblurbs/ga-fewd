0587eb70f8e6b330718505517b6f18dd3a445b46
Evan Johnson <evan@createinform.com>
2015-08-10 22:11:34 -0700

    Add GitHub scope to App Constants.

--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -1,6 +1,7 @@
 var appConstants = {
   GITHUB_API_USER: 'https://api.github.com/user?access_token=',
   GITHUB_API_REPOS: 'https://api.github.com/user/repo?access_token=',
+  GITHUB_API_SCOPE: 'user,user:email,repo',
   PARSE_APP_ID: 'AaX7jjvYNO0hc0DAjy7EHOj3jDvovR4z7TVPHOyq',
   PARSE_MASTER_KEY: '7K2v6JmT1bUJvBKTjEFGfMmkQT8j38WRbuXmUgMA',
   REST_API_KEY: 'U48iJwZuCQj46B697tH4EyTiAKWiCDp22Q4AbHDI',
