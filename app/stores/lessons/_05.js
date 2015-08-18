46a27331dc36e2a71fb49eab0751187cb33d199d
Evan Johnson <evan@createinform.com>
2015-08-17 21:55:52 -0700

    Start 5th update to website.

---
+++ app/stores/lessons/_05.js
@@ -0,0 +1,32 @@
+var _05 = {
+  title: "CSS Layouts Lab",
+  description: "This lesson will primarily be lab time. We will spend some time going over the previous layouts challenge - make sure everyone understands how to complete the various layouts. We will then review what Pseudo Classes are. Lastly, we will start working on the next assignment. You will be building Relaxr a blog!",
+  slides: "/05-layouts-lab.md",
+  number: "05",
+  date: '8.18.15',
+  Homework: [
+    {
+      title: "Coding: Relaxr Blog",
+      url: "https://github.com/ga-students/SEA-FEWD-5/tree/master/Week_03_Layout/Assignment",
+      urlText: "Relaxr Blog Assignment",
+      description: "Try and create each layout in the PDF linked above. Remember the CSS float property can have a value of left or right. To clear floats, you can set the CSS property clear to left, right, or both."
+    },
+    {
+      title: "Reading",
+      url: "https://css-tricks.com/pseudo-class-selectors/",
+      urlText: "CSS Tricks: Meet the Pseudo Class Selector",
+      description: "This artcile covers how the Pseudo Class selector can be used. It should supplement our discussion well."
+    },
+    {
+      title: "Reading",
+      url: "https://github.com/getify/You-Dont-Know-JS/blob/master/up%20&%20going/README.md#you-dont-know-js-up--going",
+      urlText: "You Don't Know JS: Up & Going",
+      description: "By "
+    }
+  ]
+};
+
+module.exports = _05;
+
+
+
