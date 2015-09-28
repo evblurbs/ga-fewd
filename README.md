2411a9edfc4a7a681ba486afbfbd6e696482a944
Evan Johnson <johnson.evan1@gmail.com>
2015-09-27 22:34:00 -0700

    Cleanup main repository, so individual classes can be moved to branches.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# sea-fewd
-A React and Parse app for the FEWD course at General Assembly.
+# FEWD Course Website
+
+This website is built to be a tool for students taking the [Front-End Web Development](https://generalassemb.ly/education/front-end-web-development) course at [General Assembly](https://generalassemb.ly). It is built with [React](https://facebook.github.io/react/) and [Parse](https://parse.com/). Slides for the individual lessons are created with [markdown and reveal.js](https://github.com/webpro/reveal-md).
--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -6,10 +6,10 @@ var appConstants = {
   GITHUB_API_EMAILS: 'https://api.github.com/user/emails?access_token=',
   GITHUB_API_SCOPE: 'user,user:email,repo',
   GITHUB_UA: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
-  PARSE_APP_ID: 'AaX7jjvYNO0hc0DAjy7EHOj3jDvovR4z7TVPHOyq',
-  PARSE_MASTER_KEY: '7K2v6JmT1bUJvBKTjEFGfMmkQT8j38WRbuXmUgMA',
-  REST_API_KEY: 'U48iJwZuCQj46B697tH4EyTiAKWiCDp22Q4AbHDI',
-  PARSE_JS_KEY: 'XgsBCS2Xsb4fEqLIEpz7eD6TGviBVrHR87hdJW6r'
+  PARSE_APP_ID: '',
+  PARSE_MASTER_KEY: '',
+  REST_API_KEY: '',
+  PARSE_JS_KEY: ''
 };
 
 module.exports = appConstants;
