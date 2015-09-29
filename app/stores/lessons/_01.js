7f49b95c6cbc9b33e228bf94c066c432085f30c1
Brian Cama <briancama@gmail.com>
2015-09-29 13:13:39 -0700

    Added Lesson 1 Links/Homework

---
+++ app/stores/lessons/_01.js
@@ -0,0 +1,58 @@
+var _01 = {
+	title: "GitHub and HTML",
+	description: "In this lesson, we will go over GitHub - how our class will use it and how it's used throughout the industry. After setting up GitHub, we'll jump into HTML. This will be a review if you went through Dash or had some experience with HTML in the past. We will cover the structure of HTML and how it is meant to be declarative of your content. Depending how fast we go through the content, we may cover other ways to use Git and GitHub, the course website and structure, and any general questions.",
+	slides: "/01-basics-html.md",
+	number: "01",
+	date: '9.28.15',
+	Homework: [
+		{
+			title: "Coding",
+			url: "https://dash.generalassemb.ly/",
+			urlText: "Dash",
+			description: "If you didn't get a chance to finish the pre-work, now is your chance! Dash teaches HTML, CSS, and Javascript through fun projects you can do in your browser. Each task should be pretty straight forward - telling you directly what to code."
+		},
+		{
+			title: "Coding: Cookie Recipe",
+			url: "https://github.com/ga-students/SEA-FEWD-5/tree/master/Week_01_Basics/01_html_basics/starter_code/cookie_recipe",
+			urlText: "Starter Code",
+			description: "Create an HTML document from the text in the starter code. The text should be wrapped with HTML tags that you find to be appropriate. Remember, HTML is supposed to be declarative. Ask yourself what is each piece of content in relationship to the structure of the whole HTML page."
+		},
+		{
+			title: "Reading",
+			url: "https://github.com/ga-students/SEA-FEWD-5/raw/master/Week_00_GitHub/Getting_Started_With_GitHub.pdf",
+			urlText: "Getting Started with GitHub",
+			description: "If you haven't read through the pre-work on GitHub, make sure to review this PDF. It can also be found in our classes repository."
+		}
+	],
+
+	Supplemental: [
+	    {
+			title: "Video",
+			url: "https://css-tricks.com/video-screencasts/58-html-css-the-very-basics/",
+			urlText: "The VERY Basics",
+			description: "This screencast is somewhat old, but not a whole lot has changed with the basics of HTML and CSS. You'll notice how the DOCTYPE tag has gotten easier to implement from the screencast."
+		},
+		{
+			title: "Git Command Line",
+			url: "https://git-scm.com/",
+			urlText: "https://git-scm.com/",
+			description: "If you'd like to use Git via the command line, download Git here. There should be a link on the right side that matches the OS you use."
+		},
+		{
+			title: "Coding",
+			url: "https://www.codeschool.com/courses/try-git",
+			urlText: "Try Git",
+			description: "Code School published a free tutorial on using Git. It is similar to Dash, in that you code along with the lesson. This will help you understand the terminology and various concepts more. Please note that you'll need to install Git to use the command line tools. These command line tools would replace the Git app we downloaded in class."
+		}
+	],
+	
+	Links: [
+		{
+			url: "https://github.com/ga-students/FEWD-SEA-6",
+			urlText: "Student Repository",
+			description: "This is the Student Repository for the class. Make sure to have a clone of this repository on your computer. On occasion new material will be added (i.e. starter code, resources, etc.) throughout the class, and you'll need to sync/pull the updates from the repository to your local repository."
+		}
+	]
+};
+
+module.exports = _01;
