7a97bcae2f93a933b2cb63d1df584c4bf6134978
Brian Cama <briancama@gmail.com>
2015-09-30 14:19:05 -0700

    Adding Lesson 02

--- app/stores/lessonStore.js
+++ app/stores/lessonStore.js
@@ -2,9 +2,11 @@
 // for example:
 // var _01 = require('./lessons/_01');
 var _01 = require('./lessons/_01');
+var _01 = require('./lessons/_02');
 
 var _lessons = [];
 _lessons.push(_01);
+_lessons.push(_02);
 // push lessons to _lessons array
 // for example:
 // _lessons.push(_01);
