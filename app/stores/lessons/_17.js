b6c40f5356b7938ec1985cdfd62c05dc9bdf513f
Brian Cama <briancama@gmail.com>
2015-12-02 17:26:38 -0800

    Add Lesson 18

---
+++ app/stores/lessons/_17.js
@@ -0,0 +1,32 @@
+var _17 = {
+  title: "Bootstrap",
+  description: "In this lesson we introduce the Bootstrap CSS framework and go over what exactly a framework provides and dive into using a few of the features and components included in Twitter Bootstrap.",
+  slides: "/18-bootstrap.md",
+  number: "18",
+  date: '12.02.15',
+  Homework: [
+    {
+      title: "Final Project",
+      url: "https://github.com/ga-students/FEWD-SEA-6/blob/master/Final_Project/final_project_requirements.md",
+      urlText: "Starter Code",
+      description: "Heading into the final few weeks of class and your main focus should be getting yourr final projects up and completed. Be sure to look over the final project requirements to make sure you are hitting all of them."
+    }
+  ],
+  Supplemental: [
+    {
+      title: "Coding",
+      url: "https://www.codecademy.com/courses/web-beginner-en-yjvdd/0/1",
+      urlText: "Bootstrap CodeAcademy",
+      description: "This codeacademy course introduces some basic web design using the bootstrap framework"
+    },
+    {
+      title: "Documentation",
+      url: "https://www.getbootstrap.com",
+      urlText: "Bootstrap Docs",
+      description: "You can find all of the bootstrap documentation and demos/examples here."
+    }
+
+  ]
+};
+
+module.exports = _17;
