82c8cc72865290a0727ee191367a33e4ffd18d26
Evan Johnson <evajohns@amazon.com>
2015-12-05 12:57:38 -0800

    Update nav login link.

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -56,7 +56,7 @@ var Nav = React.createClass({
             {this.renderWelcome()}
             <ul>
               <li>
-                <a href="/login2">Login</a>
+                <a href="/fewd7login">Login</a>
               </li>
             </ul>
           </nav>
