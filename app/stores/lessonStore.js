ea85c9825fbd9d36c61e3a4c1823188150b9f4aa
Evan Johnson <johnson.evan1@gmail.com>
2016-02-10 18:17:12 -0800

    Add 16th lesson.

--- app/stores/lessonStore.js
+++ app/stores/lessonStore.js
@@ -17,6 +17,7 @@ var _12 = require('./lessons/_12');
 var _13 = require('./lessons/_13');
 var _14 = require('./lessons/_14');
 var _15 = require('./lessons/_15');
+var _16 = require('./lessons/_16');
 
 var _lessons = [];
 // push lessons to _lessons array
@@ -25,6 +26,7 @@ var _lessons = [];
 // lessons are listed in ascending order
 // of the array's index
 
+_lessons.push(_16);
 _lessons.push(_15);
 _lessons.push(_14);
 _lessons.push(_13);
---
+++ app/stores/lessons/_16.js
@@ -0,0 +1,43 @@
+var _16 = {
+  title: "Form Basics",
+  description: "In this lesson we will go over the basics of forms. Forms allow you to collect data from your users. Since we are only doing the front-end implementation of the forms, we won’t be able to make that data persist beyond a single session. The majority of our class will be spent in lab, where everyone will work to add a form to the Relaxr website. If you finish the form, you can begin your homework, which is to make the Relaxr website responsive.",
+  slides: "/16-fewd7-form-basics.md",
+  number: "16",
+  date: '2.10.16',
+  Homework: [
+    {
+      title: "Coding",
+      url: "https://github.com/ga-students/FEWD-SEA-7/tree/master/Week_07_Multiple_Screens/Assignment/starter_code",
+      urlText: "Starter Code for Relaxr",
+      description: "To make sure you have a firm grasp of responsive design and media queries, please make the Relaxr website responsive. You can use your previous files for the Relaxr project, or start with the starter code linked above."
+    },
+    {
+      title: "Coding",
+      url: "https://github.com/ga-students/FEWD-SEA-7/tree/master/Week_08_forms_and_things/Assignment/starter_code",
+      urlText: "Starter Code for Relaxr Form",
+      description: "Add a form to the Relaxr website for users to register. This will allow you to implement the form basics that we covered in class. You can use your previous files for the Relaxr project, or start with the starter code linked above."
+    }
+  ],
+  Supplemental: [
+    {
+      title: "Coding",
+      url: "https://thimble.mozilla.org/",
+      urlText: "Thimble",
+      description: "Thimble is a site to help you learn HTML, CSS, and JavaScript. It was craeted by Mozilla, who is a great resource for learning."
+    },
+    {
+      title: "Reading",
+      url: "https://css-tricks.com/resolution-specific-stylesheets/",
+      urlText: "Different Stylesheets for Differently Sized Browser Windows",
+      description: "CSS Tricks published an article on using media queries to provide different styles to different size screens."
+    },
+    {
+      title: "Reading",
+      url: "http://webdesign.tutsplus.com/tutorials/comprehensive-guide-when-to-use-em-vs-rem--cms-23984",
+      urlText: "Comprehensive Guide: When to Use EM vs. REM",
+      description: "This article goes into more detail about the use of EM and REM. Please read this if you plan on using those units. I recommend using REM, especially for responsive designs."
+    }
+  ]
+};
+
+module.exports = _16;
