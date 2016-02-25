d78bdb89a1c9b3122b3d6ba4e2d77e3815a7be05
Brian Cama <briancama@gmail.com>
2016-02-25 00:37:22 -0800

    Add Parse Keys

--- app/constants/appConstants.js
+++ app/constants/appConstants.js
@@ -6,10 +6,10 @@ var appConstants = {
   GITHUB_API_EMAILS: 'https://api.github.com/user/emails?access_token=',
   GITHUB_API_SCOPE: 'user,user:email,repo',
   GITHUB_UA: 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
-  PARSE_APP_ID: '',
-  PARSE_MASTER_KEY: '',
-  REST_API_KEY: '',
-  PARSE_JS_KEY: ''
+  PARSE_APP_ID: 'iEQAlDH0RcSqWQ7YBsdyomtbgAoNVzGZDK9NcmfM',
+  PARSE_MASTER_KEY: 'znimGVzNJjIhxvMZzBA8Sblj9hDzdpW0p0vew8m5',
+  REST_API_KEY: 'fqrJmDT3KDA3Zfk9vnKFBQ8ngSgw0u7muIrDJ5Wj',
+  PARSE_JS_KEY: 'UBvxzXpPtbndXk9lW1Tm0e3L0xDRbSfzVzHteIJa'
 };
 
 module.exports = appConstants;
