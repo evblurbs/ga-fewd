a0e3e1ed33eb46dda2d1505eed19cc83a0741c69
Evan Johnson <johnson.evan1@gmail.com>
2016-02-25 23:14:38 -0800

    only add session data if it exists.

--- app/utils/sessionUtils.js
+++ app/utils/sessionUtils.js
@@ -1,12 +1,13 @@
 var sessionUtils = {
   createNewSession: function(sessionToken, serverResponse){
-    serverResponse.setHeader('Set-Cookie', 'session-token=' + sessionToken);
-    serverResponse.setHeader('Location', '/');
-    serverResponse.setHeader('Content-Type', 'text/plain');
-    serverResponse.statusCode = 302;
+    if(sessionToken !== 'undefined') {
+      serverResponse.setHeader('Set-Cookie', 'session-token=' + sessionToken);
+      serverResponse.setHeader('Location', '/');
+      serverResponse.setHeader('Content-Type', 'text/plain');
+      serverResponse.statusCode = 302;
+    }
     serverResponse.end();
   }
 };
 
 module.exports = sessionUtils;
-
