f07a5de2989ee499a38e07f7fc7e677228203b8b
Evan Johnson <johnson.evan1@gmail.com>
2016-02-25 23:29:52 -0800

    Rederict untill a session is found.

--- app/utils/sessionUtils.js
+++ app/utils/sessionUtils.js
@@ -2,8 +2,10 @@ var sessionUtils = {
   createNewSession: function(sessionToken, serverResponse){
     if(sessionToken != 'undefined') {
       serverResponse.setHeader('Set-Cookie', 'session-token=' + sessionToken);
+      serverResponse.setHeader('Location', '/fewd8login');
+    } else {
+      serverResponse.setHeader('Location', '/');
     }
-    serverResponse.setHeader('Location', '/');
     serverResponse.setHeader('Content-Type', 'text/plain');
     serverResponse.statusCode = 302;
     serverResponse.end();
