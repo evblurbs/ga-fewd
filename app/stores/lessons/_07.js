c55f54afffc61a64fbb6fc309f3010648030146e
Brian Cama <briancama@gmail.com>
2016-03-14 16:47:59 -0700

    Add Lesson 07

--- app/stores/lessons/_07.js
+++ app/stores/lessons/_07.js
@@ -1,35 +1,29 @@
 var _07 = {
   title: "JavaScript Intro",
-  description: "In this lesson, we are going to dive into JavaScript. We will go over what it is, and what it is used for. We will go over syntax, variables, functions, objects.",
-  slides: "/06-fewd7-javascript-intro.md",
+  description: "In this lesson, we are going to introduce you to programming and getting used to programmatic thinking. We will also introduce you to JavaScript and how it's used.",
+  slides: "/07-javascript-intro.md",
   number: "07",
-  date: '1.6.16',
+  date: '03.14.16',
   Homework: [
     {
-      title: "Coding: Startup Matchmaker",
-      url: "https://github.com/ga-students/FEWD-SEA-7/tree/master/Week_03_Layout/06_HTML_CSS_Lab/starter_code/startup_matchmaker",
-      urlText: "Startup Matchmaker",
-      description: "At this point, you should be able to take a mock and start creating the HTML/CSS. This assignment is no different. Find the PNG file that shows the Startup Machmaker. Your task will be to build the HTML and CSS for this website."
-    },
-    {
       title: "Coding",
-      url: "https://www.codecademy.com/tracks/javascript",
-      urlText: "Code Academy: JavaScript",
-      description: "By 1.4.16, go through the tutorials on Code Academy on JavaScript."
+      url: "https://github.com/ga-students/FEWD-SEA-8/tree/master/Week_04_Intro_Programming/Assignment",
+      urlText: "Week 4 Assignment",
+      description: "This weeks assignment we will be back at it with our Relaxr Blog adding some interactivity with the help of JavaScript."
     },
     {
       title: "Reading",
       url: "https://github.com/getify/You-Dont-Know-JS/blob/master/up%20&%20going/README.md#you-dont-know-js-up--going",
       urlText: "You Don't Know JS: Up & Going",
-      description: "By 1.4.16, please read chapters one and two of this book. It will be very helpful as we dive into JavaScript and programming."
+      description: "Please read chapters one and two of this book. It will be very helpful as we dive into JavaScript and programming."
     }
   ],
   Supplemental: [
     {
-      title: "Reading",
-      url: "https://www.codecademy.com/blog/1707",
-      urlText: "Your Guide to Semicolons in JavaScript",
-      description: "This article will help you understand when to use semicolons."
+      title: "Coding",
+      url: "https://www.codecademy.com/tracks/javascript",
+      urlText: "Code Academy: JavaScript",
+      description: "By 1.4.16, go through the tutorials on Code Academy on JavaScript."
     }
   ]
 };
