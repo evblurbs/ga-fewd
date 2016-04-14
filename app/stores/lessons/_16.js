1ec05840196de18a365c16331b04bdfedbc76292
Brian Cama <briancama@gmail.com>
2016-04-13 17:40:59 -0700

    Add Lesson 16

--- app/stores/lessons/_16.js
+++ app/stores/lessons/_16.js
@@ -1,41 +1,29 @@
 var _16 = {
-  title: "Form Basics",
-  description: "In this lesson we will go over the basics of forms. Forms allow you to collect data from your users. Since we are only doing the front-end implementation of the forms, we won’t be able to make that data persist beyond a single session. The majority of our class will be spent in lab, where everyone will work to add a form to the Relaxr website. If you finish the form, you can begin your homework, which is to make the Relaxr website responsive.",
-  slides: "/16-fewd7-form-basics.md",
+  title: "To Do List",
+  description: "In this lesson we will go over event delegation and then use this technique to build a to do list application.",
+  slides: "/16-event-delegation.md",
   number: "16",
-  date: '2.10.16',
+  date: '04.13.16',
   Homework: [
     {
       title: "Coding",
-      url: "https://github.com/ga-students/FEWD-SEA-7/tree/master/Week_07_Multiple_Screens/Assignment/starter_code",
-      urlText: "Starter Code for Relaxr",
-      description: "To make sure you have a firm grasp of responsive design and media queries, please make the Relaxr website responsive. You can use your previous files for the Relaxr project, or start with the starter code linked above."
-    },
-    {
-      title: "Coding",
-      url: "https://github.com/ga-students/FEWD-SEA-7/tree/master/Week_08_forms_and_things/Assignment/starter_code",
+      url: "https://github.com/ga-students/FEWD-SEA-8/tree/master/Week_08_forms_and_things/Assignment/starter_code",
       urlText: "Starter Code for Relaxr Form",
       description: "Add a form to the Relaxr website for users to register. This will allow you to implement the form basics that we covered in class. You can use your previous files for the Relaxr project, or start with the starter code linked above."
+    },
+    {
+      title: "Final Project",
+      url: "https://github.com/ga-students/FEWD-SEA-8/blob/master/Final_Project/final_project_requirements.md",
+      urlText: "Starter Code",
+      description: "Heading into the final few weeks of class and your main focus should be getting yourr final projects up and completed. Be sure to look over the final project requirements to make sure you are hitting all of them."
     }
   ],
-  Supplemental: [
-    {
-      title: "Coding",
-      url: "https://thimble.mozilla.org/",
-      urlText: "Thimble",
-      description: "Thimble is a site to help you learn HTML, CSS, and JavaScript. It was craeted by Mozilla, who is a great resource for learning."
-    },
+  Links: [
     {
       title: "Reading",
-      url: "https://css-tricks.com/resolution-specific-stylesheets/",
-      urlText: "Different Stylesheets for Differently Sized Browser Windows",
-      description: "CSS Tricks published an article on using media queries to provide different styles to different size screens."
-    },
-    {
-      title: "Reading",
-      url: "http://webdesign.tutsplus.com/tutorials/comprehensive-guide-when-to-use-em-vs-rem--cms-23984",
-      urlText: "Comprehensive Guide: When to Use EM vs. REM",
-      description: "This article goes into more detail about the use of EM and REM. Please read this if you plan on using those units. I recommend using REM, especially for responsive designs."
+      url: "http://jqfundamentals.com/chapter/events",
+      urlText: "jQuery event fundamentals",
+      description: "This article will go over all you need to know about JavaScript events using jQuery"
     }
   ]
 };
